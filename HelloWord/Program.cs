ac7af86abdcdb0240ae92ebe56caa0bc87917683
Alan <lnjnr9215@gmail.com>
2023-02-26 19:26:49 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+
+
+            if(a > b)
+            {
+                Console.WriteLine("É verdade");
+            }else if (a == a)
+            {
+                Console.WriteLine("Executou ElseIf");
+            }
+            else
+            {
+                Console.WriteLine("É mentira");
+            }
+
+
+                Console.ReadLine();
+            
+        }
+    }
+}
